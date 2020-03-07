b04c97aadc99c0169db66e15868468222e44a4cf
Lmj <601674759@qq.com>
2020-03-07 16:24:42 +0800

    添加日期项目

---
+++ Assets/_7.Date/Scripts/Date.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Date : MonoBehaviour {
+
+    public float year;
+    public int month;
+
+    void OnEnable() {
+        Debug.Log(GetDay(year, month));
+    }
+
+    private int GetDay(float year, int month) {
+        int day = 0;
+        bool isLeapYear = false;
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                isLeapYear = true;
+            }
+        } else if (year % 4 == 0) {
+            isLeapYear = true;
+        } else {
+            isLeapYear = false;
+        }
+        switch (month) {
+            case 1:
+                day = 31;
+                break;
+            case 2:
+                if (isLeapYear) day = 29;
+                else day = 28;
+                break;
+            case 3:
+                day = 31;
+                break;
+            case 4:
+                day = 30;
+                break;
+            case 5:
+                day = 31;
+                break;
+            case 6:
+                day = 30;
+                break;
+            case 7:
+                day = 31;
+                break;
+            case 8:
+                day = 31;
+                break;
+            case 9:
+                day = 30;
+                break;
+            case 10:
+                day = 31;
+                break;
+            case 11:
+                day = 30;
+                break;
+            case 12:
+                day = 31;
+                break;
+        }
+        return day;
+    }
+}
