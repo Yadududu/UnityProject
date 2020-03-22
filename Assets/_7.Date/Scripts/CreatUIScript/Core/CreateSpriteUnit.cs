72e015b11ad836ccf8854c8a2f2c4252a26ba423
Lmj <601674759@qq.com>
2020-03-23 00:07:07 +0800

    添加自动生成ui脚本功能

---
+++ Assets/_7.Date/Scripts/CreatUIScript/Core/CreateSpriteUnit.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+//导出脚本的模版
+public class CreateSpriteUnit {
+    public string classname;
+    public string template = @"
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class @ClassName {   
+@fields
+
+    public void OnAwake(GameObject viewGO){
+@body1
+    }
+
+    public void OnDestroy(){
+@body2
+    }
+}
+";
+    //缓存的所有子对象信息
+    public List<UIInfo> evenlist = new List<UIInfo>();
+    /// <summary>
+    /// 把拼接好的脚本写到本地。
+    /// （自己可以个窗口支持改名和选择路径，真实工程里是带这些功能的）
+    /// </summary>
+    public void WtiteClass() {
+        bool flag = true;
+        bool throwOnInvalidBytes = false;
+        UTF8Encoding encoding = new UTF8Encoding(flag, throwOnInvalidBytes);
+        bool append = false;
+        if (File.Exists(Application.dataPath + "/" + classname + ".cs")) {
+            EditorUtility.DisplayDialog("警告", classname + ".cs文件已存在,请先删除" + classname + ".cs或者修改文件名再生成脚本", "确定");
+        } else {
+            StreamWriter writer = new StreamWriter(Application.dataPath + "/" + classname + ".cs", append, encoding);
+            writer.Write(GetClasss());
+            writer.Close();
+            AssetDatabase.Refresh();
+        }
+    }
+    //脚本拼接
+    public string GetClasss() {
+        var fields = new StringBuilder();
+        var body1 = new StringBuilder();
+        var body2 = new StringBuilder();
+        for (int i = 0; i < evenlist.Count; i++) {
+            if (evenlist[i].field != null) fields.AppendLine("\t" + evenlist[i].field);
+            body1.AppendLine("\t\t" + evenlist[i].body1);
+            if (evenlist[i].body2 != null) body2.AppendLine("\t\t" + evenlist[i].body2);
+        }
+        template = template.Replace("@ClassName", classname).Trim();
+        template = template.Replace("@body1", body1.ToString()).Trim();
+        template = template.Replace("@body2", body2.ToString()).Trim();
+        template = template.Replace("@fields", fields.ToString()).Trim();
+        return template;
+    }
+}
---
+++ Assets/_7.Date/Scripts/CreatUIScript/Core/UIInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//子对象信息
+public class UIInfo {
+    public string field;
+    public string body1;
+    public string body2;
+    public UIInfo(string name, string typeKey, string path) {
+        field = string.Format("public {0} {1};", CreateSprite.typeMap[typeKey], name);
+        if (typeKey == "go") {
+            body1 = string.Format("{0} = viewGO.transform.Find(\"{1}\").gameObject;", name, path);
+        } else {
+            body1 = string.Format("{0} = viewGO.transform.Find(\"{1}\").GetComponent<{2}>();", name, path, CreateSprite.typeMap[typeKey]);
+        }
+        body2 = string.Format("{0} = null;", name);
+    }
+    public UIInfo(string name, string typeKey, string path, string arrName,bool isExistVar) {
+        if(!isExistVar) field = string.Format("public List<{0}> {1} = new List<{0}>();", CreateSprite.typeMap[typeKey], arrName);
+        if (typeKey == "go") {
+            body1 = string.Format("{0}.add(viewGO.transform.Find(\"{1}\").gameObject);", arrName, path);
+        } else {
+            body1 = string.Format("{0}.Add(viewGO.transform.Find(\"{1}\").GetComponent<{2}>());", arrName, path, CreateSprite.typeMap[typeKey]);
+        }
+        if (!isExistVar) body2 = string.Format("{0} = null;", arrName);
+    }
+}
